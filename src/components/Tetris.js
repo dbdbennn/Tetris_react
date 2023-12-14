8ac69aea2a87ee8d4ea577719a06b26d7da411a9
Yu Jung Eun <0hh.0553@gmail.com>
2023-12-14 22:54:58 +0900

    tetromino collision 문제 해결
    추가적으로 React.strict 모드를 제거하여 문제가 해결됨.
    1:50:47

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import { createStage } from "../gameHelpers";
+import { createStage, checkCollision } from "../gameHelpers";
 
 // styled Components
 import { StyledTetris, StyledTetrisWrapper } from "./styles/StyledTetris";
@@ -24,7 +24,9 @@ const Tetris = () => {
   console.log("re-render");
 
   const movePlayer = (dir) => {
-    updatePlayerPos({ x: dir, y: 0 });
+    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0 });
+    }
   };
 
   const startGame = () => {
